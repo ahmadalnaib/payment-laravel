85471bfe92d6c2c930c34d87e002e788f79753f4
ahmedhabeeb73 <alnaib888@gmail.com>
2021-02-01 16:42:39 +0100

    getting a payment method token

--- resources/views/subscriptions/checkout.blade.php
+++ resources/views/subscriptions/checkout.blade.php
@@ -8,17 +8,21 @@
                     <div class="card-header">{{ __('Checkout') }}</div>
 
                     <div class="card-body">
-                       <form action="" method="post">
+                       <form action="{{route('subscriptions.store')}}" method="post" id="card-form">
+                           @csrf
                            <div class="form-group">
-                               <label for="name">Name on card</label>
-                               <input type="text" name="name" id="name" class="form-control">
+                               <label for="card-holder-name">Name on card</label>
+                               <input type="text" name="name" id="card-holder-name" class="form-control">
                            </div>
 
                            <div class="form-group">
                                <label for="name">Card details</label>
                                    <div id="card-element"></div>
                            </div>
-                           <button class="btn btn-primary btn-lg" id="card-button" type="submit">Pay</button>
+                           <input type="hidden" name="plan" value="{{request('plan')}}">
+                           <button class="btn btn-primary btn-lg" id="card-button" type="submit"
+                                   data-secret="{{$intent->client_secret}}"
+                           >Pay</button>
                        </form>
                     </div>
                 </div>
@@ -31,5 +35,35 @@
         const elements = stripe.elements();
         const cardElement = elements.create('card');
         cardElement.mount('#card-element');
+
+        const form=document.getElementById('card-form');
+        const cardButton=document.getElementById('card-button');
+        const cardHolderName=document.getElementById('card-holder-name');
+        form.addEventListener('submit',async (e)=>{
+             e.preventDefault();
+             cardButton.disabled=true;
+
+           const {setupIntent,error} = await stripe.confirmCardSetup(
+            cardButton.dataset.secret,{
+                payment_method:{
+                    card:cardElement,
+                    billing_details:{
+                       name:cardHolderName.value
+                    }
+                }
+                 }
+             )
+            if(error) {
+                cardButton.disabled=false
+            } else  {
+                let token=document.createElement('input')
+                token.setAttribute('type','hidden')
+                token.setAttribute('name','token');
+                token.setAttribute('value',setupIntent.payment_method)
+
+                form.appendChild(token)
+                form.submit();
+            }
+        })
     </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::group(['namespace'=>'Subscriptions'],function () {
 
   Route::get('plans',[PlanController::class,'index'])->name('subscriptions.plans');
     Route::get('/subscriptions',[SubscriptionController::class,'index'])->name('subscriptions');
+    Route::post('/subscriptions',[SubscriptionController::class,'store'])->name('subscriptions.store');
 });
